77e2e6685f07edc86ed90339a9b577bac77b2e31
David Pukalik <davidpukalik@gmail.com>
2022-05-01 09:48:14 +0200

    removed unnecessary import

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
-import axios from "axios";
 
 type Data = {
   messageStatusRes: string;
